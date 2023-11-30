83fbcbc590c11385f38950e41b6a5c6548b55d97
样子书 <1144320763@qq.com>
2023-11-30 23:45:14 +0800

    练习

--- SWY_2023_11_29/SWY_2023_11_29/test.c
+++ SWY_2023_11_29/SWY_2023_11_29/test.c
@@ -31,94 +31,143 @@
 //}
 
 
-#include <stdio.h>
-#define N 11
-void main()
-{
-    int i, j, k, n = 0, a[N][N] = {0};
-    
-    while (n <= 0 || n >= 11)
-    {
-        printf("请输入要打印的行数：\n");
-        scanf("%d", &n);
-    }
-    printf("%d行杨辉三角如下：\n", n);
-    /*********Begin*********/
-
-    //初始化二维数组
-    for (i = 0; i < N - 1; i++)
-    {
-        for (j = 0; j <= i; j++)//每行
-        {
-            a[i][0] = 1;
-            if (j == i)
-            {
-                a[i][j] = 1;
-            }
-        }
-    }
-    
-    for (i = 0; i < N - 1; i++)
-    {
-        for (j = 0; j <= i; j++)//每行
-        {
-            if (i>1&&j>0)
-            {
-                a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
-            }
-        }
-    }
-    k = i;
-    //打印
-    for (i = 0; i < n; i++)
-    {
-        for (k = n-1-i; k > 0; k--)
-        {
-            printf("   ");
-        }
-
-
-        for (j = 0; j <= i; j++)
-        {
-           
-            printf("%6d", a[i][j]);
-            
-        }
-        printf("\n");
-    }
+//#include <stdio.h>
+//#define N 11
+//void main()
+//{
+//    int i, j, k, n = 0, a[N][N] = {0};
+//    
+//    while (n <= 0 || n >= 11)
+//    {
+//        printf("请输入要打印的行数：\n");
+//        scanf("%d", &n);
+//    }
+//    printf("%d行杨辉三角如下：\n", n);
+//    /*********Begin*********/
+//
+//    //初始化二维数组
+//    for (i = 0; i < N - 1; i++)
+//    {
+//        for (j = 0; j <= i; j++)//每行
+//        {
+//            a[i][0] = 1;
+//            if (j == i)
+//            {
+//                a[i][j] = 1;
+//            }
+//        }
+//    }
+//    
+//    for (i = 0; i < N - 1; i++)
+//    {
+//        for (j = 0; j <= i; j++)//每行
+//        {
+//            if (i>1&&j>0)
+//            {
+//                a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+//            }
+//        }
+//    }
+//    k = i;
+//    //打印
+//    for (i = 0; i < n; i++)
+//    {
+//        for (k = n-1-i; k > 0; k--)
+//        {
+//            printf("   ");
+//        }
+//
+//
+//        for (j = 0; j <= i; j++)
+//        {
+//           
+//            printf("%6d", a[i][j]);
+//            
+//        }
+//        printf("\n");
+//    }
 
     
     /*********End**********/
 
 
-    return 0;
+//    return 0;
+//}
+
+
+#include<stdio.h>
+
+void input(int a[3][3])
+{
+    /*********Begin*********/
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+
+    /*********End**********/
+}
+void output(int a[3][3])
+{
+    /*********Begin*********/
+    int i = 0;
+    int j = 0;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            printf("%5d", a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+   /*********End**********/
 }
 
+void transpose(int a[3][3])
+{
+    /*********Begin*********/
+        int tmp = a[0][1];
+        a[0][1] = a[1][0];
+        a[1][0] = tmp;
+        tmp = a[0][2];
+        a[0][2] = a[2][0];
+        a[2][0] = tmp;
 
-//#include<stdio.h>
-//
-//void input(int a[3][3])
-//{
-//    /*********Begin*********/
-//
-//
-//
-//    /*********End**********/
-//}
-//void output(int a[3][3])
-//{
-//    /*********Begin*********/
-//
-//
-//
-//   /*********End**********/
-//}
-//
-//void transpose(int a[3][3])
-//{
-//    /*********Begin*********/
-//
-//
-//
-//  /*********End**********/
-//}
+        tmp = a[1][2];
+        a[1][2] = a[2][1];
+        a[2][1] = tmp;
+        printf("transposed:\n");
+        int i = 0;
+        int j = 0;
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                printf("%5d", a[i][j]);
+            }
+            printf("\n");
+        }
+
+
+      
+
+
+  /*********End**********/
+}
+int main()
+{
+    int a[3][3] = { 0 };
+    input(a);
+    output(a);
+    transpose(a);
+    return 0;
+}
---
+++ SWY_2023_11_30/SWY_2023_11_30/test.c
@@ -0,0 +1,151 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    /*********Begin*********/
+//    int arr[10] = { 0 };
+//    int i = 0;
+//    //输入
+//    for (i = 0; i < 10; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    //排序
+//    for (i = 0; i < 9; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < 9-i; j++)
+//        {
+//            if (arr[j] < arr[j + 1])
+//            {
+//                int tmp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = tmp;
+//
+//            }
+//        }
+//    }
+//    //打印
+//    for (i = 0; i < 10; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    /*********End**********/
+//    return 0;
+//}
+
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    /*********Begin*********/
+//    int n = 0;
+//    int i = 0;
+//    int a = 0;
+//    int ret = 0;//存放第一次出现的下标
+//    int arr[1000] = { 0 };
+//    scanf("%d", &n);//输入n的值
+//
+//    for (i = 0; i < n; i++)//给数组中的n个元素赋值
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    scanf("%d", &a);//输入a的值
+//
+//    for (i = 0; i < n; i++)//寻找大小为a的元素的下标
+//    {
+//        ret = -1;//如果没找到ret==-1
+//        if (arr[i] == a)
+//        {
+//            ret = i + 1;//找到了ret==下标+1
+//            break;
+//        }
+//    }
+//    printf("%d", ret);//打印结果
+//    /*********End**********/
+//    return 0;
+//}
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    /*********Begin*********/
+//    int a = 0;
+//    int b = 0;
+//    int row = 0;//存放行
+//    int col = 0;//存放列
+//    int max = 0;//存放数组最大值
+//    int arr[10][10] = { 0 };//数组初始化
+//    scanf("%d,%d", &a, &b);//确定行列数
+//
+//    int i = 0;
+//    for (i = 0; i < a; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < b; j++)
+//        {
+//            scanf("%d", &arr[i][j]);//给数组赋值
+//        }
+//        printf("\n");
+//    }
+//   
+//    for (i = 0; i < a; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < b; j++)
+//        {
+//            if (arr[i][j] > max)
+//            {
+//                max = arr[i][j];//找到最小值
+//            }
+//        }
+//    }
+//
+//    for (i = 0; i < a; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < b; j++)
+//        {
+//            if (arr[i][j] == max)
+//            {
+//                row = i + 1;
+//                col = j + 1;    //找到最小值下标
+//            }
+//        }
+//    }
+//
+//    //打印
+//    printf("Input m, n:Input %d*%d array:\n",a,b);
+//    printf("max=%d, row=%d, col=%d", max, row, col);
+//
+//    /*********End**********/
+//    return 0;
+//}
+
+#include<stdio.h>
+int main(void)
+{
+    /*********Begin*********/
+    int n = 0;
+    int m = 0;
+    
+    
+    scanf("%d", &n);//一共有几个数
+    int arr[20] = { 0 };//初始化
+    int i = 0;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", arr[i]);//赋值
+    }
+    scanf("%d", m);//要查找的数
+    int left = 0;
+    int right = n - 1;
+    while (m!=arr[(left+right)/2])
+    {
+
+    }
+    /*********End**********/
+    return 0;
+}
